1b0be01d5e740c48f84075f15cd93a10bd4e1007
David <davidsavtran@gmail.com>
2018-08-28 23:45:12 -0400

    initial commit, getting axios set up to return data

---
+++ src/App.js
@@ -0,0 +1,70 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+import Header from './components/Header/Header';
+import axios from 'axios';
+import Qs from 'qs';
+
+const apiURL = 'https://api.petfinder.com/pet.find';
+const apiKey = '03e269d9ab2bafaf6f5ace0f1ee278f1';
+
+class App extends Component {
+  componentDidMount() {
+    axios({
+      url: 'http://proxy.hackeryou.com',
+      method: 'GET',
+      dataResponse: 'json',
+      paramsSerializer: function(params) {
+        return Qs.stringify(params, { arrayFormat: 'brackets' })
+      },
+      params: {
+        reqUrl: apiURL,
+        params: {
+          key: apiKey,
+          format: 'json',
+          output: 'full',
+          location: 'Toronto, ON',
+          animal: 'dog',
+          count: 10,
+        },
+        proxyHeaders: {
+          'header_params': 'value'
+        },
+        xmlToJSON: false
+      }
+    }).then(({data}) => {
+      console.log(data);
+    })
+  }
+  render() {
+    return (
+      <div className="App">
+        <Header/>
+      </div>
+    );
+  }
+}
+
+export default App;
+
+//Get a random animal
+
+// MVP
+
+// User can submit a location (format: City, Province).
+// API will return a call with 25 (I believe it is default latest updated profile) dogs that are up for adoption in the area.
+// Display 5 dogs randomly.
+//Basic profile will be displayed: name, age, breed, gender, one photo and 
+//User can click 'Read More' to see a profile section with a description, contact information of each animal. 
+// User will be able to randomize again to display 5 dogs randomly. You might see the same twice, leave this as is. Maybe the same dog appearing will signal DESTINY!
+
+// Add a really random button to just see random adoptable pets in the database (worldwide?);
+
+
+//STRETCH
+
+//Favourite an animal, add it to Firebase. User will be able to see a list of animals they have favourited.
+
+// Filter by breed (for dogs only?)
+// Filter by type of animal (or allow toggle of Cats/Both)
+// Find other pets in the same shelter by making another call to get the Shelter list. Display the dogs in that list.
---
+++ src/components/Header/Header.js
@@ -0,0 +1,10 @@
+import React, {Component} from 'react';
+import './Header.css';
+
+const Header = () => {
+  return (
+    <h1>This is my header.</h1>
+  );
+}
+
+export default Header;
